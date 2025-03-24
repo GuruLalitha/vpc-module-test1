f178ae677a99ab3a8b404d2ec1d8382521dbf364
GuruLalitha <guru.lalitha90@gmail.com>
2025-03-24 23:10:10 +0530

    S3 PART-2

--- vpc.tf
+++ vpc.tf
@@ -6,5 +6,8 @@ module "vpc" {
 
     common_tags = var.common_tags
     vpc_tags = var.vpc_tags
-  
+    public_subnet_cidrs = var.public_subnet_cidrs
+    private_subnet_cidrs = var.private_subnet_cidrs
+    database_subnet_cidrs = var.database_subnet_cidrs
+    is_peering_required = true
 }
